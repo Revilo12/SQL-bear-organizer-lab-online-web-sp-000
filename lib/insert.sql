436111e6b241ac97131ebdce7bce389e31c271ab
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-01 16:45:17 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears (name, age, alive) VALUES ('Sergeant Brown', 1, 1);
 INSERT INTO bears (name, age, alive) VALUES ('Melissa', 1, 1);
 INSERT INTO bears (name, age, alive) VALUES ('Grinch', 1, 1);
 INSERT INTO bears (name, age, alive) VALUES ('Wendy', 1, 1);
-INSERT INTO bears (name, age, alive) VALUES ('NULL', 1, 1);
+INSERT INTO bears (name, age, alive) VALUES (NULL, 1, 1);
